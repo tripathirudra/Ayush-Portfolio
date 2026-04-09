3f542542630bcc14101c415d30026d2129c57a22
tripathirudra <tripathirudra934@gmail.com>
2026-04-09 13:33:09 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-8 text-sm opacity-70">
+      © {new Date().getFullYear()} Ayush Tripathi
+    </footer>
+  );
+}
---
+++ src/data/data.jsx
@@ -0,0 +1,33 @@
+import { Link } from "lucide-react";
+
+export const roles = [
+  "Full Stack Developer",
+  "MERN Stack Developer",
+  "Backend Engineer",
+  "Problem Solver"
+];
+
+export const projectsData = [
+  
+
+  {
+    title: <a href="https://travoss-qa8l.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-1 text-blue-400 hover:underline">TRAVOCE – Cab Booking <Link size={16} /></a>,
+    desc: "OTP-based ride booking platform with admin dashboard and payment integration.",
+    link: "https://travoss-qa8l.vercel.app/"
+  },
+  
+  {
+  title: <a href="https://nivritti-frontend.onrender.com" target="_blank" rel="noopener noreferrer" className="flex items-center gap-1 text-blue-400 hover:underline">Nivritti <Link size={16} /></a>,
+  desc: "Digital micro-lending and job matching platform recognized in HackQuest 2.0.",
+  link: "https://nivritti-frontend.onrender.com"
+}
+];    
+
+export const skillsData = [
+  { name: "React.js", level: 90 },
+  { name: "Node.js", level: 85 },
+  { name: "MongoDB", level: 80 },
+  { name: "Express.js", level: 85 },
+  { name: "JavaScript", level: 92 },
+  { name: "Java", level: 75 }
+];
